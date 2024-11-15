77b2b335c50fec8dd4ae3adfcd6de564fea20719
Antonio Vachev <vachevantonio@gmail.com>
2024-11-15 17:35:46 +0200

    Initial diff test with Pillow

---
+++ compare_images/compare.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageChops
+
+img1 = Image.open(r'C:\Users\AVachev\Code\image_comparison\source_images\IMAGE_1.png')
+img2 = Image.open(r'C:\Users\AVachev\Code\image_comparison\source_images\IMAGE_2.png')
+
+difference = ImageChops.difference(img1, img2)
+
+if difference.getbbox():
+    difference.show()
