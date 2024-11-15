e40a9b69ab8712342bd3262e3a3746dcf7edd02e
Antonio Vachev <vachevantonio@gmail.com>
2024-11-15 17:47:36 +0200

    Save image to show differences

--- compare_images/compare.py
+++ compare_images/compare.py
@@ -6,4 +6,5 @@ img2 = Image.open(r'C:\Users\AVachev\Code\image_comparison\source_images\IMAGE_2
 difference = ImageChops.difference(img1, img2)
 
 if difference.getbbox():
+    difference.save(r'C:\Users\AVachev\Code\image_comparison\difference_images\difference.png')
     difference.show()
